92cd624f47f6a0421d9320af6b2ddefef8df89bb
Megan Dotto <megan.dotto@gmail.com>
2018-03-27 02:34:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,3 +42,13 @@ end
 def sum_array(array)
   array.inject(0){|sum,x| sum + x }
 end
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index == 1
+      word
+    else
+      word+"s"
+    end 
+  end
+end 
