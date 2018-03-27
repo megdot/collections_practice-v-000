7f23f56310bf7f686953c4fd901487cf7e2371c0
Megan Dotto <megan.dotto@gmail.com>
2018-03-27 01:48:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,4 +18,10 @@ end
 
 def sort_array_char_count(array)
   array.sort {|a, b| a.length <=> b.length}
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
 end
